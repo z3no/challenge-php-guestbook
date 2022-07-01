659a19faa3318eb091c3bf079d7f0e686b016ddd
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-01 12:09:57 +0200

    changes in the constructor for the post class

--- model/Post.php
+++ model/Post.php
@@ -10,47 +10,34 @@ class Post
     private string $content;
     private string $author;
 
-    /**
-     * @param string $title
-     * @param string $date
-     * @param string $content
-     * @param string $author
-     */
-    public function __construct(string $title, string $date, string $content, string $author)
+
+    public function __construct(string $title, string $content, string $author)
     {
         $this->title = $title;
-        $this->date = $date;
+        $this->date = date("F j, Y, g:i a");
         $this->content = $content;
         $this->author = $author;
     }
 
-    /**
-     * @return string
-     */
+
     public function getTitle(): string
     {
         return $this->title;
     }
 
-    /**
-     * @return string
-     */
+
     public function getDate(): string
     {
         return $this->date;
     }
 
-    /**
-     * @return string
-     */
+
     public function getContent(): string
     {
         return $this->content;
     }
 
-    /**
-     * @return string
-     */
+
     public function getAuthor(): string
     {
         return $this->author;
