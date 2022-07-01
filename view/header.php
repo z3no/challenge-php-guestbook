645d84dbce0b4a998c81edb4f44037bbb7ddfc2b
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-01 10:53:11 +0200

    the header is born

---
+++ view/header.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Guestbook</title>
+    <link href="/css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
