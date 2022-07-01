9646b3aadb63590a971bf53fc40dd7e55b06233e
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-01 14:27:39 +0200

    created my form

--- view/view.php
+++ view/view.php
@@ -8,26 +8,29 @@ declare(strict_types=1);
     <h1>Welcome to my guestbook</h1>
 </header>
 
-<section>
+<main>
     <h2>Write a review:</h2>
 
-    <form method="post" action="../index.php">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <div>
-            <label for="title">Title:</label>
-            <input type="text" name="title" required="required" placeholder="Enter a title">
+            <label for="title">Title:
+                <input type="text" name="title" required="required" placeholder="Enter a title">
+            </label>
         </div>
 
         <div>
-            <label for="author">Author:</label>
-            <input type="text" name="author" required="required" placeholder="Enter your name">
+            <label for="author">Author:
+                <input type="text" name="author" required="required" placeholder="Enter your name">
+            </label>
         </div>
 
         <div>
-            <label for="content">Content:</label>
-            <textarea id="content" name="content" rows="10" cols="50" required="required" placeholder="Enter some content"></textarea>
+            <label for="content">Content:
+                <textarea id="content" name="content" rows="5" cols="50" required="required" placeholder="Enter some content"></textarea>
+            </label>
         </div>
 
         <button type="submit">Submit</button>
 
     </form>
-</section>
+</main>
