a39b35f4f27a41413ac53d92ce3729615a13db2b
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-01 10:52:32 +0200

    put view in a folder, made basic form structure

--- view/view.php
+++ view/view.php
@@ -1,40 +1,33 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PHP Guestbook</title>
-</head>
-<body>
-    <header>
-        <h1>Welcome to my guestbook</h1>
-    </header>
+<?php
 
-    <section>
-        <h2>Write a review:</h2>
+declare(strict_types=1);
 
-        <form method="post" action="index.php">
-            <div>
-                <label for="title">Title:</label>
-                <input type="text" name="title" required="required" placeholder="Enter a title">
-            </div>
+?>
 
-            <div>
-                <label for="author">Author:</label>
-                <input type="text" name="author" required="required" placeholder="Enter your name">
-            </div>
+<header>
+    <h1>Welcome to my guestbook</h1>
+</header>
 
-            <div>
-                <label for="content">Content:</label>
-                <textarea id="content" name="content" rows="10" cols="50" required="required" placeholder="Enter some content"></textarea>
-            </div>
+<section>
+    <h2>Write a review:</h2>
 
-            <button type="submit">Submit</button>
+    <form method="post" action="../index.php">
+        <div>
+            <label for="title">Title:</label>
+            <input type="text" name="title" required="required" placeholder="Enter a title">
+        </div>
 
-        </form>
-    </section>
+        <div>
+            <label for="author">Author:</label>
+            <input type="text" name="author" required="required" placeholder="Enter your name">
+        </div>
 
-</body>
-</html>
+        <div>
+            <label for="content">Content:</label>
+            <textarea id="content" name="content" rows="10" cols="50" required="required" placeholder="Enter some content"></textarea>
+        </div>
+
+        <button type="submit">Submit</button>
+
+    </form>
+</section>
